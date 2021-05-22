b9fc628a1d4582784b886d58f15cce46451ea3bb
shashank3656 <79498546+shashank3656@users.noreply.github.com>
2021-05-22 15:41:50 +0530

    Add files via upload

---
+++ LoadMovieData.py
@@ -0,0 +1,39 @@
+#########################################################################################
+#  Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this
+#  software and associated documentation files (the "Software"), to deal in the Software
+#  without restriction, including without limitation the rights to use, copy, modify,
+#  merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+#  permit persons to whom the Software is furnished to do so.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+#  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+#  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#########################################################################################
+from decimal import Decimal
+import json
+import boto3
+
+
+def load_movies(movies, dynamodb=None):
+    if not dynamodb:
+        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+
+    table = dynamodb.Table('Movies')
+    for movie in movies:
+        year = int(movie['year'])
+        title = movie['title']
+        info = movie['info']
+        print("Adding movie:", year, title, info)
+        table.put_item(Item=movie)
+
+
+if __name__ == '__main__':
+    with open("moviedata.json") as json_file:
+        movie_list = json.load(json_file, parse_float=Decimal)
+    load_movies(movie_list)
